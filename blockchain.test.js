70a152f5d4e0328425f656ffea0ec6d931107a29
CG <cgodlew@gmail.com>
2020-10-06 00:16:08 -0400

    243 3.17. Began adding testing for chainlink

--- blockchain.test.js
+++ blockchain.test.js
@@ -18,5 +18,21 @@ describe ('Blockchain', () => {
 
     expect(blockchain.chain[blockchain.chain.length-1].data).toEqual(newData);
   });
+
+  describe('isValidChain()', () => {
+    describe('when the chain does not start with the genesis block', () => {
+      it('returns false', () => {})
+    });
+
+    describe('when the chain does start with the genesis block and has multiple blocks', () => {
+      describe('and a lastHash reference has changed', () => {
+        it('returns false', () => {
+
+
+        });
+      });
+    });
+  });
 });
+
 
