03835bfee2fdce9c94c1fc9f3140b6e4d0552d7f
CG <cgodlew@gmail.com>
2020-10-09 00:45:05 -0400

    Added further testing to blockchain.test.js and made code more succinct

--- blockchain.test.js
+++ blockchain.test.js
@@ -2,7 +2,11 @@ const Blockchain = require('./blockchain');
 const Block = require('./block');
 
 describe ('Blockchain', () => {
-  const blockchain = new Blockchain();
+  let blockchain;
+
+  beforeEach(() => {
+    blockchain = new Blockchain()
+  });
   
   it('contains a `chain` Array instance', () => {
     expect(blockchain.chain instanceof Array).toBe(true);
@@ -21,14 +25,47 @@ describe ('Blockchain', () => {
 
   describe('isValidChain()', () => {
     describe('when the chain does not start with the genesis block', () => {
-      it('returns false', () => {})
+      it('returns false', () => {
+        blockchain.chain[0] = {data: 'fake-genesis'};
+
+        expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+      });
     });
 
     describe('when the chain does start with the genesis block and has multiple blocks', () => {
+
+      beforeEach(() => {
+        blockchain.addBlock({ data: 'Bears'});
+        blockchain.addBlock({ data: 'Beets'});
+        blockchain.addBlock({ data: 'Battle'});
+      });
+
       describe('and a lastHash reference has changed', () => {
         it('returns false', () => {
 
+          blockchain.chain[2].lastHash = 'broken-lastHash'
 
+          expect(Blockchain.isValidChain(blockchain.chain)).toBe(false); 
+        });  
+      });
+
+      describe('and the chain contains a block with an invalid field', () => {
+        it('returns false', () => {
+
+          blockchain.chain[2].data = 'bad-data';
+
+
+          expect(Blockchain.isValidChain(blockchain.chain)).toBe(false); 
+
+        });
+
+
+      });
+
+      describe('and the chain doe not contain any invalid blocks', () => {
+        it('returns false', () => {
+
+          expect(Blockchain.isValidChain(blockchain.chain)).toBe(true); 
         });
       });
     });
