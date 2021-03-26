3196ff9197177bae27673adfa4af156bf1dfb6c5
Carl Godlewski <cgodlew@gmail.com>
2021-03-26 18:57:26 -0400

    Added func to adjust difficulty of mining. Fixed difficulty variable to be const

--- block.js
+++ block.js
@@ -19,11 +19,12 @@ class Block {
   static mineBlock({ lastBlock, data }) {
     let hash, timestamp;
     const lastHash = lastBlock.hash;
-    const { difficulty } = lastBlock;
+    let { difficulty } = lastBlock;
     let nonce = 0;
 
     do {
       nonce++;
+      difficulty = Block.adjustDifficulty({originalBlock: lastBlock, timestamp});
       timestamp = Date.now();
       hash = cryptoHash(timestamp, lastHash, data, difficulty, nonce);
     } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty));
