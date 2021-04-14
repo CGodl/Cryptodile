73cd66700f5ce925dd3742bcc264074c71349334
Carl Godlewski <cgodlew@gmail.com>
2021-04-14 01:31:40 -0400

    installed bodyparser, added POST func

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ app.get('/api/blocks', (req, res) => {
   res.json(blockchain.chain);
 });
 
+app.post('/api/mine', (req, res) => {
+  const {data} = req.body;
+
+  blockchain.addBlock({data});
+  
+  res.redirect('/api/blocks');
+})
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
