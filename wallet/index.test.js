3069e7e04cbf29709ea9678f9fff4e722f698c0e
Carl Godlewski <cgodlew@gmail.com>
2021-05-19 00:21:12 -0400

    Updated invalid signature test to call new instance of Wallet class

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -34,7 +34,7 @@ describe('Wallet', () => {
         verifySignature({
           publicKey: wallet.publicKey,
           data,
-          signature: wallet.sign(data)
+          signature: new Wallet().sign(data)
         })
       ).toBe(false);
 
