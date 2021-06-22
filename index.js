aa398f2ca1ca648882e02a9ba89ecb24b0d90174
Carl Godlewski <cgodlew@gmail.com>
2021-06-22 15:47:33 -0400

    Added pubsub func to broadcast transactions and sync transactions on connect

--- index.js
+++ index.js
@@ -11,12 +11,11 @@ const app = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
-const pubsub = new PubSub({ blockchain });
+const pubsub = new PubSub({ blockchain, transactionPool });
 
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`
 
-// setTimeout(() => pubsub.broadcastChain(), 1000);
 
 app.use(express.json());
 
@@ -54,6 +53,9 @@ app.post('/api/transact', (req, res) => {
 
   transactionPool.setTransaction(transaction);
 
+  pubsub.broadcastTransaction(transaction);
+  
+
   res.json({ type: 'success', transaction });
 });
 
@@ -62,7 +64,7 @@ app.get('/api/transaction-pool-map', (req, res) => {
 });
 
 
-const syncChains = () => {
+const syncWithRootState = () => {
   request({ url: `${ROOT_NODE_ADDRESS}/api/blocks`}, (error, response, body) => {
     if (!error && response.statusCode === 200) {
       const rootChain = JSON.parse(body);
@@ -71,6 +73,15 @@ const syncChains = () => {
       blockchain.replaceChain(rootChain);
     };
   });
+
+  request({url: `${ROOT_NODE_ADDRESS}/api/transaction-pool-map`}, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const rootTransactionPoolMap = JSON.parse(body);
+
+      console.log('replace transaction pool map on a sync with', rootTransactionPoolMap);
+      transactionPool.setMap(rootTransactionPoolMap);
+    }
+  })
 };
  
 
@@ -86,7 +97,7 @@ app.listen(PORT, () => {
   console.log(`Started listening at localhost: ${PORT}`);
 
   if (PORT !==DEFAULT_PORT) {
-    syncChains();
+    syncWithRootState();
   };
 
 })
